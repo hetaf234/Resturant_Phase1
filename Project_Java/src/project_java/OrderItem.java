6175a80770c926a8ff206f16bb3069d4a2cea043
hetaf234 <hetaf.maz@gmail.com>
2025-02-12 10:19:56 +0300

    orderItem Class is done

--- Project_Java/src/project_java/OrderItem.java
+++ Project_Java/src/project_java/OrderItem.java
@@ -13,6 +13,41 @@ public OrderItem(String itemName, double price, int quantity) {
 }//end of OrderItem Constructor 
 public abstract double calculateTotal();
 
+
+
+public String toString() {
+	return "OrderItem [itemName=" + itemName + ", price=" + price + ", quantity=" + quantity + "]";
+}//to string
+public String getItemName() {
+	return itemName;
+}//getItemName
+public void setItemName(String itemName) {
+	this.itemName = itemName;
+}//setItemName
+public double getPrice() {
+	return price;
+}//getPrice()
+public void setPrice(double price) {
+	this.price = price;
+}//setPrice(double price)
+public int getQuantity() {
+	return quantity;
+}//getQuantity()
+public void setQuantity(int quantity) {
+	this.quantity = quantity;
+}//setQuantity(int quantity)
+
+
+
+
+
+
+
+
+
+
+
+
 	
 	
 }//class OrderItem
